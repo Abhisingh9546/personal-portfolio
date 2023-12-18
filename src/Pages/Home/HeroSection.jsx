6c56edc9f4e1b49b8ac5b67719c04508a9b72499
rohit kumar <rohitkumar22august20@gmail.com>
2023-12-18 15:20:16 +0530

    portfolio completed

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Abhishek</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            <b>I have 2 years experience in this field for Web Development.</b>
+            
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.jpeg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
